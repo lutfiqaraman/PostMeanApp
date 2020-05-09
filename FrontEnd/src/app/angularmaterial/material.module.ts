e163dba08cd618e7733e03f9413f682b89cc5be6
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-10 00:42:39 +0300

    using angular material

--- FrontEnd/src/app/angularmaterial/material.module.ts
+++ FrontEnd/src/app/angularmaterial/material.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
-  exports: []
+  exports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ]
 })
 export class MaterialModule { }
