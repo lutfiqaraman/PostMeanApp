cc80b4c7a2ac45de0e6124e6776063b9b81d4529
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-12 22:26:08 +0300

    fetch a dummy data as a json format

--- PostApp/Backend/app.js
+++ PostApp/Backend/app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  res.send('Express framework');
+app.use('/api/posts',(req, res, next) => {
+  const postList = [
+    {
+      id: 'fad123d3w',
+      title: 'First Post',
+      content: 'The content of the first post from the server side'
+    },
+    {
+      id: 'gfh145ert',
+      title: 'Second Post',
+      content: 'The content of the second post from the server side'
+    },
+    {
+      id: 'sfe956qrs',
+      title: 'Third Post',
+      content: 'The content of the third post from the server side'
+    }
+  ];
+
+  res.status(200).json(postList);
 });
 
 module.exports = app;
