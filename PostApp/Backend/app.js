d57f2ea830c5dea59107f4842f7564082b1141ab
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-13 11:05:13 +0300

    add cors

--- PostApp/Backend/app.js
+++ PostApp/Backend/app.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-
-  next();
-});
+app.use(cors());
 
 require("./routes/post.route")(app);
 
