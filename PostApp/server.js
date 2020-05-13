f9cfb07373b4c1665def8447ba02aad14373cdac
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-13 11:06:18 +0300

    edit server

--- PostApp/server.js
+++ PostApp/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./Backend/app');
 
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.set('port', port);
 const server = http.createServer(app);
