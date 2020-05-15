fe7457e5ccea7ffdd8e3b826eca8a22cb44ce984
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-15 19:29:31 +0300

    use a variable

--- PostApp/Backend/controllers/post.controller.js
+++ PostApp/Backend/controllers/post.controller.js
@@ -23,9 +23,9 @@ exports.getAllPosts = async (req, res) => {
 
 // Posts - Delete a post
 exports.deletePost = async (req, res) => {
-  const id = req.params.id;
+  const postID = req.params.id;
   
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
+  Post.deleteOne({ _id: postID }).then((result) => {
     res.status(200).json(result);
   });
 }
